fc8a4e704f332137d011d96a18f2bdefa87774f1
Squishy177 <2026peted05@moundsviewschools.org>
2025-09-06 00:17:28 -0500

    Update 1.2

--- achievements.js
+++ achievements.js
@@ -2,152 +2,205 @@ const achievementData=[
     {//0
         name:'First click',
         desc:'Click the button for the first time',
-        cond:function(){return clicks>=1}
+        cond:function(){return clicks>=1},
+        tags:[],
     },
     {//1
         name:'Second click',
         desc:'Click the button for the <i>second</i> time',
-        cond:function(){return clicks>=2}
+        cond:function(){return clicks>=2},
+        tags:[],
     },
     {//2
         name:'Depression',
         desc:'Reach 0.01% of your life wasted',
-        cond:function(){return Math.floor(100000*time/3600/24/365/80)/1000>=0.01}
+        cond:function(){return Math.floor(100000*time/3600/24/365/80)/1000>=0.01},
+        tags:[],
     },
     {//3
         name:'May as well try it',
         desc:'Click on the Game Log',
-        cond:function(){return false}
+        cond:function(){return false},
+        tags:[],
     },
     {//4
         name:'Question',
         desc:'Click on the the little ?',
-        cond:function(){return false}
+        cond:function(){return false},
+        tags:[],
     },
     {//5
         name:'An hour\'s worth',
         desc:'Spend an hour wasted',
-        cond:function(){return Math.floor(time/3600)>0}
+        cond:function(){return Math.floor(time/3600)>0},
+        tags:[],
     },
     {//6
         name:'An hour\'s worth',
         desc:'Spend an hour wasted',
-        cond:function(){return Math.floor(time/3600)>0}
+        cond:function(){return Math.floor(time/3600)>0},
+        tags:[],
+        icon:[0,0],
     },
     {//7
         name:'Fourth click',
         desc:'But there is one for <i>four</i>',
-        cond:function(){return clicks>=4}
+        cond:function(){return clicks>=4},
+        tags:[],
+        icon:[0,0],
     },
     {//8
         name:'Heavy tapping',
         desc:'Burn 1 calorie from clicking',
-        cond:function(){return clicks*0.00142857142>=1}
+        cond:function(){return clicks*0.00142857142>=1},
+        tags:[],
+        icon:[0,0],
     },
     {//9
         name:'Not even breaking a sweat',
         desc:'Burn 3 calories from clicking',
-        cond:function(){return clicks*0.00142857142>=3}
+        cond:function(){return clicks*0.00142857142>=3},
+        tags:[],
+        icon:[0,0],
     },
     {//10
         name:'Breaking a sweat',
         desc:'Burn 10 calories from clicking',
-        cond:function(){return clicks*0.00142857142>=10}
+        cond:function(){return clicks*0.00142857142>=10},
+        tags:[],
+        icon:[0,0],
     },
     {//11
         name:'Big Mac',
         desc:'Burn 250 calories from clicking (roughly half the amount a big mac has)',
-        cond:function(){return clicks*0.00142857142>=500}
+        cond:function(){return clicks*0.00142857142>=500},
+        tags:[],
+        icon:[0,0],
     },
     {//12
         name:'Clicking it again?',
         desc:'Click the ? a second time',
-        cond:function(){return false}
+        cond:function(){return false},
+        tags:[],
+        icon:[0,0],
     },
     {//13
         name:'Minimum Wage',
         desc:'Lose $9.20',
-        cond:function(){return Math.floor(100*9.2*time/3600)/100>=9.2}
+        cond:function(){return Math.floor(100*9.2*time/3600)/100>=9.2},
+        tags:[],
+        icon:[0,0],
     },
     {//14
         name:'What is the cause?',
         desc:'Witness 500 deaths. What is even happening?!',
-        cond:function(){return Math.floor(1.73611111111*time)>=500}
+        cond:function(){return Math.floor(1.73611111111*time)>=500},
+        tags:[],
+        icon:[0,0],
     },
     {//15
         name:'Feeling old yet?',
         desc:'Play the game for more than one hour',
-        cond:function(){return time/3600>=1}
+        cond:function(){return time/3600>=1},
+        tags:[],
+        icon:[0,0],
     },
     {//16
         name:'Gotta catch them all',
         desc:'Collect 10 achievements',
-        cond:function(){return unlocked.length>=10}
+        cond:function(){return unlocked.length>=10},
+        tags:[],
+        icon:[0,0],
     },
     {//17
         name:'Nice',
-        desc:'All but this achievement',
-        cond:function(){return unlocked.length-1==achievementData.length}
+        desc:'All but this achievement.',
+        cond:function(){return unlocked.length-1==achievementData.length},
+        tags:['secret'],
+        icon:[0,0],
     },
     {//18
         name:'What have we found...',
-        desc:'Uncover dark secrets',
-        cond:function(){return false}
+        desc:'Uncover the "Unlocks" tab',
+        cond:function(){return false},
+        tags:[],
+        icon:[0,0],
     },
     {//19
         name:'Last on earth. Not',
         desc:'Witness 7 billion deaths',
-        cond:function(){return Math.floor(1.73611111111*time)>=7000000000}
+        cond:function(){return Math.floor(1.73611111111*time)>=7000000000},
+        tags:[],
+        icon:[0,0],
     },
     {//20
         name:'Milestone Novice',
         desc:'Get 10 milestones',
-        cond:function(){return totalMilestones>=10}
+        cond:function(){return totalMilestones>=10},
+        tags:[],
+        icon:[0,0],
     },
     {//21
         name:'Milestone Knight',
         desc:'Get 20 milestones',
-        cond:function(){return totalMilestones>=20}
+        cond:function(){return totalMilestones>=20},
+        tags:[],
+        icon:[0,0],
     },
     {//22
         name:'Milestone Master',
         desc:'Get 30 milestones',
-        cond:function(){return totalMilestones>=30}
+        cond:function(){return totalMilestones>=30},
+        tags:[],
+        icon:[0,0],
     },
     {//23
         name:'Milestone Grandmaster',
         desc:'Get 50 milestones',
-        cond:function(){return totalMilestones>=50}
+        cond:function(){return totalMilestones>=50},
+        tags:[],
+        icon:[0,0],
     },
     {//24
         name:'Barely Lucky',
         desc:'Click and get lucky (0.5%)',
-        cond:function(){return false}
+        cond:function(){return false},
+        tags:[],
+        icon:[0,0],
     },
     {//25
         name:'Lucky',
         desc:'Click and get lucky (0.1%)',
-        cond:function(){return false}
+        cond:function(){return false},
+        tags:[],
+        icon:[0,0],
     },
     {//26
         name:'Quite Lucky',
         desc:'Click and get lucky (0.05%)',
-        cond:function(){return false}
+        cond:function(){return false},
+        tags:[],
+        icon:[0,0],
     },
     {//27
         name:'Very Lucky',
-        desc:'Click and get lucky (0.01%)',
-        cond:function(){return false}
+        desc:'Click and get lucky (0,01%)',
+        cond:function(){return false},
+        tags:[],
+        icon:[0,0],
     },
     {//28
         name:'Luck in its Essence',
         desc:'Click and get lucky (0.005%)',
-        cond:function(){return false}
+        cond:function(){return false},
+        tags:[],
+        icon:[0,0],
     },
     {//29
         name:'Stars Aligned',
         desc:'Gain 2 "Lucky" achievements with one click',
         cond:function(){return false},
-        tags:['secret'],
+        tags:['secret','hidden'],
+        icon:[0,0],
     },
 ]
--- main.js
+++ main.js
@@ -1,3 +1,41 @@
+var lastΔ = Date.now()
+var loadedImages={};
+var tab = 1;
+
+
+function preloadImage(url)
+{
+    let img=new Image();
+    img.src=url;
+    loadedImages[url]=img;
+}
+function getImage(url) {
+    return loadedImages[url];
+}
+function getSfx(url) {
+    return loadedSfx[url];
+}
+const preloadImages=[
+    'secret1.png',
+    'icons.png',
+    'milestones.png',
+];
+
+var loadedSfx={};
+const preloadSfxs=[
+
+];
+function preloadSfx(url){
+    let aud=new Audio();
+    aud.src=url;
+    loadedSfx[url]=aud;
+}
+function playSfx(url) {
+    loadedSfx[url].play();
+}
+
+
+
 var clicks=0;
 var time=0;
 var log=['Welcome to the <b>Time Waster</b>','Click on the "click" button to start']
@@ -6,7 +44,7 @@ var unlocked=[];
 var milestones=[
     {
         unlocked:0,
-        starting:200,
+        starting:100,
         mult:2,
         txt:function(i){return 'You have witnessed <b>'+i+'</b> deaths'},
     },
@@ -17,6 +55,13 @@ var milestones=[
         txt:function(i){return 'You have clicked <b>'+i+'</b> times'},
     },
 ]
+function visible(id,conditionifon) {
+  if (conditionifon){
+    document.getElementById(id).removeAttribute("hidden",'');
+  }else{
+    document.getElementById(id).setAttribute('hidden','');
+  }
+}
 var totalMilestones=0;
 function unlock(i){
     if (!unlocked.includes(i)){
@@ -29,67 +74,130 @@ function unlock(i){
 function updateMilestones(item,variable){
     let i = milestones[item];
     let value=i.starting*(i.mult**i.unlocked);
-    if (variable>=value){
+    if (i.unlocked<=13 && variable>=value){
         log.push("<span style='color:gray'>"+i.txt(value)+"</span>");
         i.unlocked++;
         totalMilestones++;
-    }
-        
-        
+    }     
 }
 function clickButton(){
     clicks+=1;
     if (clicks==3) log.push("<span style='color:gray'>Nice try, but there is no achievement for 3 clicks</span>");
     if (clicks>3){
         let amt = 0
-        if (Math.random()<0.005) unlock(24); amt++;
-        if (Math.random()<0.001) unlock(25); amt++;
-        if (Math.random()<0.0005) unlock(26); amt++;
-        if (Math.random()<0.0001) unlock(27); amt++;
-        if (Math.random()<0.00005) unlock(28); amt++;
+        if (Math.random()<0.005){unlock(24); amt++;}
+        if (Math.random()<0.001){unlock(25); amt++;}
+        if (Math.random()<0.0005){unlock(26); amt++;}
+        if (Math.random()<0.0001){ unlock(27); amt++;}
+        if (Math.random()<0.00005){ unlock(28); amt++;}
         if (amt>1) unlock(29);
     }
 }
+
+document.addEventListener("keydown", function(evt) {
+    // evt rocks yeah?!
+    if (evt.key=='Enter') {
+        evt.preventDefault();
+    }
+}, false);
+
+
 function tick(){
-    document.querySelector('#clicks').innerHTML='Estimated Calories Burned: '+clicks*0.00142857142;
-    document.querySelector('#time').innerHTML='Time Wasted: '+Math.floor(time/3600/24)+' Days, '+Math.floor(time/3600)%24+' Hours, '+Math.floor(time/60)%60+' Minutes, '+Math.floor(time)%60+' seconds';
+    let Δ = Date.now() - lastΔ;
+    lastΔ = Date.now();
+    document.getElementById('tab1').className = (tab==1)?'tab':'tab-closed';
+    document.getElementById('tab2').className = (tab==2)?'tab':'tab-closed';
+    document.getElementById('tab3').className = (tab==3)?'tab':'tab-closed';
+    document.getElementById('tab4').className = (tab==4)?'tab':'tab-closed';
+    let unlockedCount = 0;
+    for (i=0;i<unlocked.length;i++){
+        if (achievementData[unlocked[i]].tags.indexOf('secret')==-1) unlockedCount++;
+    }
+    let achievementCount = 0;
+    for (i=0;i<achievementData.length;i++){
+        if (achievementData[i].tags.indexOf('secret')==-1) achievementCount++;
+    }
+    document.getElementById('complete').innerHTML='Achievements completed: '+Math.floor(1000*(unlockedCount/achievementCount))/10+'% ('+unlockedCount+' out of '+achievementCount+')';
 
+    visible('clicks',tab==1);
+    visible('time',tab==1);
+    visible('breath',tab==1);
+    visible('death',tab==1);
+    visible('getajob',tab==1);
+    visible('total',tab==1);
+    visible('percent',tab==1);
 
-    document.querySelector('#breath').innerHTML='Estimated Breaths taken: '+Math.floor(18*time/60);
-    document.querySelector('#death').innerHTML='Number of people that died since you started: '+Math.floor(1.73611111111*time);
-    document.querySelector('#getajob').innerHTML='Amount of money that you could have been earning if you had a job: $'+Math.floor(100*9.2*time/3600)/100;
-    document.querySelector('#total').innerHTML='Total clicks clicked: '+clicks;
-    document.querySelector('#percent').innerHTML='Percent of your estimated life wasted: '+Math.floor(10000000*time/3600/24/365/80)/100000+'%';
-    document.querySelector('#complete').innerHTML='Achievements completed: '+Math.floor(1000*(unlocked.length/achievementData.length))/10+'% ('+unlocked.length+' out of '+achievementData.length+')';
+    visible('unlocks',tab==2);
+
+    visible('patch',tab==3);
+    
+    // let string = '';
+    // if (tab==2){
+    //     
+    //     string += '<br><h2>Milestones:</h2><br>';
+    //     for (i=0;i<milestones.length;i++){
+    //         string += '<div class="achievement'+((i%2==1)?'-locked':'')+'"><img onclick="" src="milestones.png" class="" style="object-position: 0px 0px;"></div>';
+    //     }
+    // }
+
+    // document.getElementById('unlocks').innerHTML = string;
+
+    document.getElementById('clicks').innerHTML='Estimated Calories Burned: '+clicks*0.00142857142;
+    document.getElementById('time').innerHTML='Time Wasted: '+Math.floor(time/3600/24)+' Days, '+Math.floor(time/3600)%24+' Hours, '+Math.floor(time/60)%60+' Minutes, '+Math.floor(time)%60+' seconds';
+    document.getElementById('breath').innerHTML='Estimated Breaths taken: '+Math.floor(18*time/60);
+    document.getElementById('death').innerHTML='Number of people that died since you started: '+Math.floor(1.73611111111*time);
+    document.getElementById('getajob').innerHTML='USD that you could have been earning if you had a job: $'+Math.floor(100*9.2*time/3600)/100;
+    document.getElementById('total').innerHTML='Total clicks clicked: '+clicks;
+    document.getElementById('percent').innerHTML='Percent of your estimated life wasted: '+Math.floor(10000000*time/3600/24/365/80)/100000+'%';
+    
+    
+    
     let str=''
     let offset=Math.max(0,log.length-7)
     for (i=0;i<Math.min(7,log.length);i++){
         str+='<p class="txt" style="color:black">'+log[i+offset]+'</p>'
         
     }
-    document.querySelector('#commentary').innerHTML=str;
-
-    let str='';
-    for (i = 0; i < achievementData.length; i++){
-        str += '<img src="secret1.png" style="width:50px;height:50px;">'
-
-    }
-    document.getElementById('unlocks').innerHTML = str;
-
-
-
-
+    document.getElementById('commentary').innerHTML=str;
 
     for (i=0; i<achievementData.length; i++){    
         if (achievementData[i].cond()) unlock(i);
         //This script runs through all of the achievements and checks if they are unlocked
     }
+
+    const canvas = document.getElementById("unlocks");
+    // if (canvas.getContext) {
+    //     const ctx = canvas.getContext("2d");
+    //     ctx.clearRect(0,0,canvas.width,canvas.height);
+    //     ctx.font = '30px Serif';
+    //     ctx.fillStyle = 'black';
+
+    //     let y = 0;
+        
+    //     for (i=0;i<achievementData.length;i++){
+    //         ctx.drawImage(getImage('icons.png'), 0, 0, 24, 24, (i%10)*48, Math.floor(i/10)*64, 32, 32);
+            
+    //     }
+    //     ctx.fillText("Milestones", 10, Math.floor(achievementData.length/10)*64+20);
+    //     for (i=0;i<milestones.length;i++){
+    //         ctx.drawImage(getImage('milestones.png'), 0, 0, 320, 320, i*70, Math.floor(achievementData.length/10)*64+Math.floor(i/10)*70+40, 64, 64);
+    //     }
+        
+    // }
+
+
     updateMilestones(0,Math.floor(1.73611111111*time));
     updateMilestones(1,clicks);
-
-
-    time = Date.now() - timestart;
+    
+    time += Δ / 1000;
 
 }
+for (var i = 0; i < preloadImages.length; i++){
+    preloadImage(preloadImages[i]);
+}
+for (var i = 0; i < preloadSfxs.length; i++){
+    preloadSfx(preloadSfxs[i]);
+}
+setInterval(tick,1000/60)
 
-setInterval(tick,1000/60) // 60 fps. I think
+
